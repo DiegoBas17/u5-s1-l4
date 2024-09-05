6aadb45b14a1177242354fd40e4bc965dc635885
DiegoBas17 <167855623+DiegoBas17@users.noreply.github.com>
2024-09-05 14:09:26 +0200

    Initial commit

---
+++ src/main/java/diego/basili/u5_s1_l4/U5S1L4Application.java
@@ -0,0 +1,13 @@
+package diego.basili.u5_s1_l4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5S1L4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5S1L4Application.class, args);
+	}
+
+}
---
+++ src/test/java/diego/basili/u5_s1_l4/U5S1L4ApplicationTests.java
@@ -0,0 +1,13 @@
+package diego.basili.u5_s1_l4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5S1L4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
